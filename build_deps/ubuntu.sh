11bd13bf6d9418e30c2e4f59b0004c40c59e9379
Johan Kjölhede <GiGurra@users.noreply.github.com>
2016-02-06 19:02:23 +0100

    Update ubuntu.sh

--- build_deps/ubuntu.sh
+++ build_deps/ubuntu.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 apt-get install mesa-common-dev 
-apt-get install mesa-utils-extra, libgl1-mesa-dev, libglapi-mesa
-apt-get install mesa-utils-extra, libgl1-mesa-dev libglapi-mesa
 apt-get install mesa-utils-extra libgl1-mesa-dev libglapi-mesa
 apt-get install libxrandr-dev
 apt-get install libxinerama-dev 
