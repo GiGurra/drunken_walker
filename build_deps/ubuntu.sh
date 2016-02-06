0b449a52f1deef0de0cec4d781446aadd14b9699
Johan Kjölhede <gigurra@gmail.com>
2016-02-06 10:43:24 +0100

    Add build deps script for ubuntu

---
+++ build_deps/ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get install mesa-common-dev 
+apt-get install mesa-utils-extra, libgl1-mesa-dev, libglapi-mesa
+apt-get install mesa-utils-extra, libgl1-mesa-dev libglapi-mesa
+apt-get install mesa-utils-extra libgl1-mesa-dev libglapi-mesa
+apt-get install libxrandr-dev
+apt-get install libxinerama-dev 
+apt-get install libxcursor-dev
+apt-get install build-essential
+
